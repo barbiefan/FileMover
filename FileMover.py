df607ecca4a91b13c2448081b45ad7961bef8bdf
@obey <georgecarlin770@gmail.com>
2020-12-27 13:20:52 +0300

    finished testing

--- FileMover.py
+++ FileMover.py
@@ -13,16 +13,10 @@ while True:
 
     for file in content:
         name = file[file.rfind('/')+1:]
-        print(name)
         ext = file[file.rfind('.')+1:]
-        print(ext)
         if ext in config.keys():
-            print(f'{ext} in config')
             if config[ext]:
-                print(config[ext])
                 os.rename(file, config[ext]+name)
-                print(file)
-                print(config[ext]+name)
         else:
             path = easygui.enterbox(f'unsigned extension {ext}\ntype a path for files with this extension\n(empty to ignore {ext}):')
             config[ext] = None if path == '' else path
