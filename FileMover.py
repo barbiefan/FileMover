6c2db47f1d5d1a28199321ac9f7c331393ddbb1b
@obey <georgecarlin770@gmail.com>
2020-12-27 04:08:53 +0300

    improved extensions associations\nadded prompt asking a path for a new extension

--- FileMover.py
+++ FileMover.py
@@ -2,12 +2,31 @@ import json
 import os
 import time
 import glob
+import easygui
 
 with open('move.config', 'r') as file:
     config = json.load(file)
+
 while True:
-    for key, value in list(config.items())[2:]:
-        files = glob.glob(config['from']+key)
-        for file in files:
-            os.rename(file, value+'/'+file[file.rfind('/')+1:])
+
+    content = glob.glob(config['from']+'*.*')
+
+    for file in content:
+        name = file[file.rfind('/')+1:]
+        print(name)
+        ext = file[file.rfind('.')+1:]
+        print(ext)
+        if ext in config.keys():
+            print(f'{ext} in config')
+            if config[ext]:
+                print(config[ext])
+                os.rename(file, config[ext]+name)
+                print(file)
+                print(config[ext]+name)
+        else:
+            path = easygui.enterbox(f'unsigned extension {ext}\ntype a path for files with this extension\n(empty to ignore {ext}):')
+            config[ext] = None if path == '' else path
+            with open('move.config', 'w') as file:
+                json.dump(config, file)
+
     time.sleep(config['sleep'])
