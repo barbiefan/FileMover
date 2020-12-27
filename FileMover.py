863091a593135462ee5fd54c95273b90e245fff4
@obey <georgecarlin770@gmail.com>
2020-12-27 14:11:39 +0300

    minor path handling rework

--- FileMover.py
+++ FileMover.py
@@ -12,13 +12,13 @@ while True:
     content = glob.glob(config['from']+'*.*')
 
     for file in content:
-        name = file[file.rfind('/')+1:]
-        ext = file[file.rfind('.')+1:]
+        name = os.path.split(file)[-1]
+        ext = name.split('.')[-1]
         if ext in config.keys():
             if config[ext]:
-                os.rename(file, config[ext]+name)
+                os.rename(file, os.path.join(config[ext], name))
         else:
-            path = easygui.enterbox(f'unsigned extension {ext}\ntype a path for files with this extension\n(empty to ignore {ext}):')
+            path = easygui.enterbox(f'unknown extension {ext}\ntype a path for files with this extension\n(empty to ignore {ext}):')
             config[ext] = None if path == '' else path
             with open('move.config', 'w') as file:
                 json.dump(config, file)
